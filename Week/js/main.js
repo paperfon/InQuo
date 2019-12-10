2c321e0ba2662e382d7d09bfb4dc3aa4c5e643c2
Rod Paperfon <admin@paperfon.com>
2019-12-10 14:06:24 +0100

    Responsiveness and adding today class

--- Week/js/main.js
+++ Week/js/main.js
@@ -3,32 +3,36 @@ document.getElementById("week-number").textContent = moment().isoWeek();
 let startDay = moment().startOf('isoWeek');
 let endDay = moment().endOf('isoWeek');
 
-let countDaysOfWeek = function(startDay, endDay) {
-  let dates = [];
+let countDaysOfWeek = function (startDay, endDay) {
+    let dates = [];
 
-  let firstDate = moment(startDay).startOf('day');
-  let lastDate = moment(endDay).startOf('day');
+    let firstDate = moment(startDay).startOf('day');
+    let lastDate = moment(endDay).startOf('day');
 
-  do {
-    dates.push(firstDate.toDate());
+    do {
+        dates.push(firstDate.toDate());
 
-    var dateElement = document.createElement('p');
-    dateElement.id = moment(firstDate.toDate()).format("YYYY-MM-DD");
-    dateElement.textContent = moment(firstDate.toDate()).format("DD MMM YYYY");
-    
-    var dayElement = document.createElement('span');
-    dayElement.className = moment(firstDate.toDate()).format("dddd");
-    dayElement.textContent = moment(firstDate.toDate()).format("dddd");
+        var dateElement = document.createElement('p');
+        dateElement.id = moment(firstDate.toDate()).format("YYYY-MM-DD");
+        dateElement.textContent = moment(firstDate.toDate()).format("DD MMM YYYY");
+        let today = moment().format("YYYY-MM-DD");
+        if (dateElement.id == today) {
+            dateElement.classList.add("today");
+        }
 
-    var spacer = document.createElement('br');
+        var dayElement = document.createElement('span');
+        dayElement.className = moment(firstDate.toDate()).format("dddd");
+        dayElement.textContent = moment(firstDate.toDate()).format("dddd");
 
-    dayElement.appendChild(spacer);
-    dateElement.insertBefore(dayElement, dateElement.firstChild);
+        var spacer = document.createElement('br');
 
-    document.getElementById("week-days").appendChild(dateElement);
-  } while (firstDate.add(1, 'days').diff(lastDate) <= 0);
+        dayElement.appendChild(spacer);
+        dateElement.insertBefore(dayElement, dateElement.firstChild);
 
-  return dates;
+        document.getElementById("week-days").appendChild(dateElement);
+    } while (firstDate.add(1, 'days').diff(lastDate) <= 0);
+
+    return dates;
 }
 
 console.log(countDaysOfWeek(startDay, endDay));
